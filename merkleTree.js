0bb5f584b3b0d6464b05fa6f8570d709171599fc
Philipp Piwo <github@piwo.space>
2021-09-07 20:58:29 +0200

    refactor as class

--- merkleTree.js
+++ merkleTree.js
@@ -1,53 +1,53 @@
-function MerkleTree(leaves, hashFunction) {
+class MerkleTree {
+
+  constructor(leaves, hashFunction) {
     this.hashFunction = hashFunction;
     this.leaves = this.hashLeaves(leaves);
     this.tree = [this.leaves];
     this.createTree(this.leaves);
-}
+  }
 
-MerkleTree.prototype = {
+  getTree() {
+    return this.tree;
+  }
 
-    getTree: function () {
-        return this.tree;
-    },
+  getRootHash() {
+    return this.tree[this.tree.length - 1][0];
+  }
 
-    getRootHash: function () {
-        return this.tree[this.tree.length - 1][0];
-    },
+  hashLeaves(leaves) {
+    return leaves.map(leaf => this.hashFunction(leaf))
+  }
 
-    hashLeaves: function (leaves) {
-        return leaves.map(leaf => this.hashFunction(leaf))
-    },
+  getIndex(leaf) {
+    const hashedLeaf = this.hashFunction(leaf);
+    const index = this.leaves.indexOf(hashedLeaf);
 
-    getIndex: function (leaf) {
-        const hashedLeaf = this.hashFunction(leaf);
-        const index = this.leaves.indexOf(hashedLeaf);
+    if (index === -1) throw new Error("leaf not found");
+    return index
+  }
 
-        if (index === -1) throw new Error("leaf not found");
-        return index
-    },
+  getProof(leaf) {
+    return this.tree.slice(0, this.tree.length - 1).reduce(({index, proof}, layer) => {
+      const isRightNode = index % 2 !== 0;
+      proof.push(isRightNode ? layer[index - 1] : layer[index + 1] || layer[index]);
+      return {index: (index / 2) | 0, proof: proof}
+    }, {index: this.getIndex(leaf), proof: []}).proof;
+  }
 
-    getProof: function (leaf) {
-        return this.tree.slice(0, this.tree.length - 1).reduce(({index, proof}, layer) => {
-            const isRightNode = index % 2 !== 0;
-            proof.push(isRightNode ? layer[index - 1] : layer[index + 1] || layer[index]);
-            return {index: (index / 2) | 0, proof: proof}
-        }, {index: this.getIndex(leaf), proof: []}).proof;
-    },
-
-    createTree: function (leaves) {
-        const items = [];
-        for (var i = 0; i < leaves.length; i += 2) {
-            const left = leaves[i];
-            const right = leaves[i + 1] || left;
-            items.push(this.hashFunction(left + right));
-        }
-
-        if (leaves.length !== 1) {
-            this.tree.push(items);
-            this.createTree(items);
-        }
+  createTree(leaves) {
+    const items = [];
+    for (var i = 0; i < leaves.length; i += 2) {
+      const left = leaves[i];
+      const right = leaves[i + 1] || left;
+      items.push(this.hashFunction(left + right));
     }
-};
+
+    if (leaves.length !== 1) {
+      this.tree.push(items);
+      this.createTree(items);
+    }
+  }
+}
 
 module.exports = MerkleTree;
